d0fb8d4fa1ea715379631343bd436945eaecba3e
Haley Hoech <haley.hoech@hhi.fraunhofer.de>
2020-09-17 17:41:09 +0200

    conflicts

--- exec.sh
+++ exec.sh
@@ -10,13 +10,8 @@
 cmdargs=$1
 
 hyperparameters=' [{
-<<<<<<< HEAD
 	"dataset" : ["cifar10"], 
 	"distill_dataset" : ["stl10"],
-=======
-	"dataset" : ["mnist"], 
-	"distill_dataset" : ["emnist"],
->>>>>>> f6c8f47059f4cdbf9f577c25c02e4f5f0cd0eaac
 	"net" : ["lenet_cifar"],
 	
 
